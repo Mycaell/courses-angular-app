6d2ebb6a91965068b88aafddd10525d23aa99606
mycaell de oliveira <oliveiramycaell@gmail.com>
2023-10-20 21:15:09 -0300

    add toolbar in main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
