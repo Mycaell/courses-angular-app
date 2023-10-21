0da2cd34b7e7620ea16688247dcc2bf3b8cba0e0
mycaell de oliveira <oliveiramycaell@gmail.com>
2023-10-21 00:18:31 -0300

    add lazy routing

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CoursesListComponent } from './courses-list/courses-list.component';
+
+const routes: Routes = [
+  { path: '', component: CoursesListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
