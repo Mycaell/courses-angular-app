46d9dd8338af434ada38659da7bbb3a08023a22c
mycaell de oliveira <oliveiramycaell@gmail.com>
2023-10-28 14:27:23 -0300

    create enum of routes

---
+++ src/app/courses/courses-routes.ts
@@ -0,0 +1,7 @@
+export enum CoursesRoutes {
+
+  HOME = '',
+  CREATE = 'novo',
+  EDIT = 'editar/:id',
+
+}
--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CourseFormComponent } from './component/course-form/course-form.component';
 import { CourseHomeComponent } from './component/course-home/course-home.component';
+import { CoursesRoutes } from './courses-routes';
 
 
 const routes: Routes = [
-  { path: '', component: CourseHomeComponent },
-  { path: 'novo', component: CourseFormComponent },
+  { path: CoursesRoutes.HOME, component: CourseHomeComponent },
+  { path: CoursesRoutes.CREATE, component: CourseFormComponent },
+  { path: CoursesRoutes.EDIT, component: CourseFormComponent }
 ];
 
 @NgModule({
