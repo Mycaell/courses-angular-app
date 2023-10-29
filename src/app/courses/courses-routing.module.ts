4ae29f9240dbf7d614fad9d0d8d71278090fe854
mycaell de oliveira <oliveiramycaell@gmail.com>
2023-10-29 05:02:47 -0300

    create course resolver

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CourseFormComponent } from './component/course-form/course-form.component';
 import { CourseHomeComponent } from './component/course-home/course-home.component';
 import { CoursesRoutes } from './courses-routes';
+import { courseResolver } from './guard/course.resolver';
 
 
 const routes: Routes = [
   { path: CoursesRoutes.HOME, component: CourseHomeComponent },
-  { path: CoursesRoutes.CREATE, component: CourseFormComponent },
-  { path: CoursesRoutes.EDIT, component: CourseFormComponent }
+  { path: CoursesRoutes.CREATE, component: CourseFormComponent, resolve: { course: courseResolver } },
+  { path: CoursesRoutes.EDIT, component: CourseFormComponent, resolve: { course: courseResolver } }
 ];
 
 @NgModule({
---
+++ src/app/courses/guard/course.resolver.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { ResolveFn } from '@angular/router';
+
+import { Observable, of } from 'rxjs';
+
+import { Course } from '../model/course';
+import { CourseService } from '../service/course.service';
+
+export const courseResolver: ResolveFn<Course> = (
+  route,
+  state,
+  service: CourseService = inject(CourseService)): Observable<Course> => {
+
+  if (route.params && route.params['id']) {
+    return service.findById(route.params['id']);
+  }
+
+  return of({ id: 0, name: '', category: '' });
+};
