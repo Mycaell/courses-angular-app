265ca6b4b9b27254b43cdb72a1e52dbca17f8d2a
mycaell de oliveira <oliveiramycaell@gmail.com>
2023-10-30 07:08:38 -0300

    create toast service with sweetalert2

---
+++ src/app/shared/service/toast.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import Swal, { SweetAlertIcon } from 'sweetalert2';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  private readonly toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 4000,
+    timerProgressBar: true,
+    didOpen: (element) => {
+      element.addEventListener('mouseenter', Swal.stopTimer);
+      element.addEventListener('mouseleave', Swal.resumeTimer);
+    }
+  });
+
+  open(message: string, icon: SweetAlertIcon): void {
+    this.toast.fire({
+      icon: icon,
+      title: message
+    });
+  }
+}
